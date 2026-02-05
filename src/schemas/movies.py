782fe869549620d58f487e65c1e345ed24e46c7e
Artem <artemp2208@gmail.com>
2026-02-05 15:16:29 +0200

    feature: add schemas for movies

---
+++ src/schemas/movies.py
@@ -0,0 +1,118 @@
+from typing import List, Optional
+from uuid import UUID
+
+from pydantic import BaseModel, Field
+
+
+class GenreSchema(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        from_attributes = True
+
+
+class StarSchema(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        from_attributes = True
+
+
+class DirectorSchema(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        from_attributes = True
+
+
+class CertificationSchema(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        from_attributes = True
+
+
+class MovieCreate(BaseModel):
+    name: str
+    year: int
+    time: int
+
+    imdb: float
+    votes: int
+
+    meta_score: Optional[float] = None
+    gross: Optional[float] = None
+
+    description: str
+    price: Optional[float] = None
+
+    certification_id: int
+
+    genre_ids: List[int] = Field(default_factory=list)
+    star_ids: List[int] = Field(default_factory=list)
+    director_ids: List[int] = Field(default_factory=list)
+
+
+class MovieUpdate(BaseModel):
+    name: Optional[str] = None
+    year: Optional[int] = None
+    time: Optional[int] = None
+
+    imdb: Optional[float] = None
+    votes: Optional[int] = None
+
+    meta_score: Optional[float] = None
+    gross: Optional[float] = None
+
+    description: Optional[str] = None
+    price: Optional[float] = None
+
+    certification_id: Optional[int] = None
+
+    genre_ids: Optional[List[int]] = None
+    star_ids: Optional[List[int]] = None
+    director_ids: Optional[List[int]] = None
+
+
+class MovieListItem(BaseModel):
+    id: int
+    uuid: UUID
+
+    name: str
+    year: int
+    time: int
+
+    imdb: float
+    price: Optional[float] = None
+
+    class Config:
+        from_attributes = True
+
+
+class MovieRead(BaseModel):
+    id: int
+    uuid: UUID
+
+    name: str
+    year: int
+    time: int
+
+    imdb: float
+    votes: int
+    meta_score: Optional[float]
+    gross: Optional[float]
+
+    description: str
+    price: Optional[float]
+
+    certification: CertificationSchema
+    genres: List[GenreSchema]
+    stars: List[StarSchema]
+    directors: List[DirectorSchema]
+
+    class Config:
+        from_attributes = True
