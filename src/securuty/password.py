8c72b14b730c838eda50a74afd562711b0333d63
Vorongor <mr.maddarknes@gmail.com>
2026-02-05 14:33:44 +0200

    Correct code style

--- src/securuty/password.py
+++ src/securuty/password.py
@@ -1,4 +1,5 @@
 import bcrypt
+from typing import cast
 
 
 def hash_password(password: str) -> str:
@@ -6,7 +7,7 @@ def hash_password(password: str) -> str:
     pwd_bytes = password.encode("utf-8")
 
     salt = bcrypt.gensalt()
-    hashed = bcrypt.hashpw(pwd_bytes, salt)
+    hashed = cast(bytes, bcrypt.hashpw(pwd_bytes, salt))
 
     return hashed.decode("utf-8")
 
@@ -14,8 +15,9 @@ def hash_password(password: str) -> str:
 def verify_password(plain_password: str, hashed_password: str) -> bool:
 
     try:
-        return bcrypt.checkpw(
-            plain_password.encode("utf-8"), hashed_password.encode("utf-8")
-        )
+        return cast(bool, bcrypt.checkpw(
+            plain_password.encode("utf-8"),
+            hashed_password.encode("utf-8")
+        ))
     except Exception:
         return False
