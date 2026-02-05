5fef2de47143548dab52a545364e026bd258627a
Anastasiia Duzhynska <a.duzhynska@gmail.com>
2026-02-05 19:32:23 +0200

    feat: implemented order schemas

---
+++ src/schemas/orders.py
@@ -0,0 +1,48 @@
+import decimal
+from datetime import datetime
+from typing import List, Optional
+
+from pydantic import BaseModel, ConfigDict, field_validator
+
+from src.databases.models.orders import StatusEnum
+
+
+class OrderItemBaseSchema(BaseModel):
+    movie_id: int
+
+
+class OrderBaseSchema(BaseModel):
+    pass
+
+
+class OrderCreateSchema(OrderBaseSchema):
+    movie_ids: List[int]
+
+    @field_validator("movie_ids")
+    @classmethod
+    def check_not_empty(cls, v):
+        if not v:
+            raise ValueError("Order must contain at least one movie ID")
+        return v
+
+
+class OrderItemReadSchema(OrderItemBaseSchema):
+    id: int
+    price_at_order: decimal.Decimal
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class OrderReadSchema(OrderBaseSchema):
+    id: int
+    user_id: int
+    created_at: datetime
+    status: StatusEnum
+    total_amount: Optional[decimal.Decimal]
+    items: List[OrderItemReadSchema]
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class OrderStatusUpdateSchema(BaseModel):
+    status: StatusEnum
