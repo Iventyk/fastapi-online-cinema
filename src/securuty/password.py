3981d4f7bb68af9b4a05b4b1f95e175f1d29e45c
Vorongor <mr.maddarknes@gmail.com>
2026-02-05 14:36:22 +0200

    Correct code style

--- src/securuty/password.py
+++ src/securuty/password.py
@@ -15,9 +15,11 @@ def hash_password(password: str) -> str:
 def verify_password(plain_password: str, hashed_password: str) -> bool:
 
     try:
-        return cast(bool, bcrypt.checkpw(
-            plain_password.encode("utf-8"),
-            hashed_password.encode("utf-8")
-        ))
+        return cast(
+            bool,
+            bcrypt.checkpw(
+                plain_password.encode("utf-8"), hashed_password.encode("utf-8")
+            ),
+        )
     except Exception:
         return False
