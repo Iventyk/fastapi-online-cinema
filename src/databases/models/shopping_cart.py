0f8428a9783f864ceb0c252be7ad597bd85d908a
Oleh Skibinskyi <oleg.sk2002@gmail.com>
2026-02-05 15:58:17 +0200

    feature: add models for shopping car
    Additional:
     - add relations to movie models and user models

---
+++ src/databases/models/shopping_cart.py
@@ -0,0 +1,61 @@
+from typing import List, TYPE_CHECKING
+from datetime import datetime
+
+from sqlalchemy import (
+    ForeignKey,
+    UniqueConstraint,
+    func,
+    DateTime,
+)
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+    relationship,
+)
+
+from src.databases.models.base import Base
+
+
+if TYPE_CHECKING:
+    from src.databases.models import UserModel, Movie
+
+
+class Cart(Base):
+    __tablename__ = "carts"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"),
+        unique=True,
+        nullable=False
+    )
+
+    user: Mapped["UserModel"] = relationship(back_populates="cart")
+    items: Mapped[List["CartItem"]] = relationship(
+        back_populates="cart",
+        cascade="all, delete-orphan"
+    )
+
+class CartItem(Base):
+    __tablename__ = "cart_items"
+    __table_args__ = (
+        UniqueConstraint("cart_id", "movie_id", name="uq_cart_item_movie"),
+    )
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    cart_id: Mapped[int] = mapped_column(
+        ForeignKey("carts.id", ondelete="CASCADE"),
+        nullable=False
+    )
+    movie_id: Mapped[int] = mapped_column(
+        ForeignKey("movies.id", ondelete="CASCADE"),
+        nullable=False
+    )
+    added_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False
+    )
+
+    cart: Mapped["Cart"] = relationship(back_populates="items")
+    movie: Mapped["Movie"] = relationship(back_populates="cart_items")
