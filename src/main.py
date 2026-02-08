13c65b8c21f03b05d12887ead40c5eaf4b21a515
Oleh Skibinskyi <oleg.sk2002@gmail.com>
2026-02-08 17:09:09 +0200

    feature: add swagger docs
    Additional:
      - reformat code according to linters suggestions
      - switch some error answers in validators and crud

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import logging
 import secrets
 from dotenv import load_dotenv
 from contextlib import asynccontextmanager
-from typing import AsyncIterator
+from typing import AsyncIterator, Any
 
 from fastapi import FastAPI, Request, status, Depends, HTTPException
 from fastapi.encoders import jsonable_encoder
@@ -11,7 +11,7 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi.openapi.utils import get_openapi
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
-from starlette.responses import JSONResponse
+from starlette.responses import JSONResponse, HTMLResponse
 
 from src.databases import Base
 from src.databases.dev_engine import AsyncSessionLocal, engine
@@ -28,15 +28,21 @@ logging.basicConfig(
 security = HTTPBasic()
 
 
-def check_docs_permissions(credentials: HTTPBasicCredentials = Depends(security)):
+def check_docs_permissions(
+    credentials: HTTPBasicCredentials = Depends(security),
+) -> str:
     """
     Checking login and password to enter documentation.
     """
-    DOCS_USERNAME = os.getenv("DOCS_LOGIN")
-    DOCS_PASSWORD = os.getenv("DOCS_PASSWORD")
+    DOCS_USERNAME = os.getenv("DOCS_LOGIN", "admin")
+    DOCS_PASSWORD = os.getenv("DOCS_PASSWORD", "password")
 
-    correct_username = secrets.compare_digest(credentials.username, DOCS_USERNAME)
-    correct_password = secrets.compare_digest(credentials.password, DOCS_PASSWORD)
+    correct_username = secrets.compare_digest(
+        credentials.username, DOCS_USERNAME
+    )
+    correct_password = secrets.compare_digest(
+        credentials.password, DOCS_PASSWORD
+    )
 
     if not (correct_username and correct_password):
         raise HTTPException(
@@ -58,21 +64,25 @@ async def lifespan(app: FastAPI) -> AsyncIterator[None]:
 
 
 app = FastAPI(
-    lifespan=lifespan,
-    docs_url=None,
-    redoc_url=None,
-    openapi_url=None
+    lifespan=lifespan, docs_url=None, redoc_url=None, openapi_url=None
 )
 
 app.include_router(api_v1_router)
 
 
 @app.get("/docs", include_in_schema=False)
-async def get_swagger_documentation(username: str = Depends(check_docs_permissions)):
-    return get_swagger_ui_html(openapi_url="/openapi.json", title="Online Cinema API Docs")
+async def get_swagger_documentation(
+    username: str = Depends(check_docs_permissions),
+) -> HTMLResponse:
+    return get_swagger_ui_html(
+        openapi_url="/openapi.json", title="Online Cinema API Docs"
+    )
+
 
 @app.get("/openapi.json", include_in_schema=False)
-async def get_open_api_endpoint(username: str = Depends(check_docs_permissions)):
+async def get_open_api_endpoint(
+    username: str = Depends(check_docs_permissions),
+) -> dict[str, Any]:
     return get_openapi(
         title="FastAPI Online Cinema",
         version="1.0.0",
--- src/routers/shopping_cart.py
+++ src/routers/shopping_cart.py
@@ -29,13 +29,62 @@ from src.schemas import (
 )
 from src.securuty.utils import get_current_user
 
-shopping_cart_router = APIRouter(prefix="/cart", tags=["Carts"])
+shopping_cart_router = APIRouter(prefix="/cart", tags=["Shopping Cart"])
 
 
 @shopping_cart_router.post(
     "/{user_id}/",
     status_code=status.HTTP_201_CREATED,
     response_model=MovieInCartSchema,
+    summary="Add a movie to the shopping cart",
+    description="""
+    Adds a specific movie to the user's shopping cart based on the provided `movie_id`. 
+
+    **Validation Rules:**
+    - The **User** must exist.
+    - The **Movie** must exist in the database.
+    - The item must **not already be in the cart**.
+    - The user must **not have already purchased** this movie (Status: PAID or PENDING).
+
+    **Permissions:**
+    - Users can only add items to their **own** cart.
+    - **Moderators** and **Admins** can add items to any user's cart.
+    """,  # noqa: 80
+    responses={
+        400: {
+            "description": "Business logic validation failed",
+            "content": {
+                "application/json": {
+                    "examples": {
+                        "user_not_exist": {
+                            "summary": "user not found",
+                            "value": {"detail": "User not found."},
+                        },
+                        "movie_not_found": {
+                            "summary": "Movie not found",
+                            "value": {"detail": "Movie not found"},
+                        },
+                        "already_exists": {
+                            "summary": "Item already in cart",
+                            "value": {"detail": "Cart item already exists"},
+                        },
+                        "repeat_purchase": {
+                            "summary": "Already purchased",
+                            "value": {"detail": "Repeat purchase not allowed"},
+                        },
+                    }
+                }
+            },
+        },
+        403: {
+            "description": "Permission denied",
+            "content": {
+                "application/json": {
+                    "example": {"detail": "Not enough permission."}
+                }
+            },
+        },
+    },
 )
 async def create_cart_item(
     user_id: int,
@@ -43,6 +92,15 @@ async def create_cart_item(
     db: Annotated[AsyncSession, Depends(get_db)],
     cart_item: CartItemCreateSchema,
 ) -> MovieInCartSchema:
+    """
+    Controller for adding a movie to the cart.
+
+    Arguments:
+        user_id (int): The ID of the target user owner of the cart.
+        authenticated_user (CurrentUser): The user performing the request (from JWT). # noqa: 80
+        db (AsyncSession): Database session.
+        cart_item (CartItemCreateSchema): Payload containing the `movie_id`.
+    """
     try:
         return await create_new_cart_item(
             db=db,
@@ -66,42 +124,61 @@ async def create_cart_item(
 
 
 @shopping_cart_router.delete(
-    "/{user_id}/{cart_item_id}",
-    status_code=status.HTTP_204_NO_CONTENT,
-)
-async def delete_cart_item(
-    user_id: int,
-    authenticated_user: Annotated[CurrentUser, Depends(get_current_user)],
-    db: Annotated[AsyncSession, Depends(get_db)],
-    cart_item_id: int,
-) -> None:
-    try:
-        await remove_cart_item(
-            db=db,
-            user_id=user_id,
-            authenticated_user=authenticated_user,
-            cart_item_id=cart_item_id,
-        )
-
-    except (UserNotExist, CartItemDoesNotExist) as error:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail=str(error)
-        )
-    except (UserPermissionDenied,) as error:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN, detail=str(error)
-        )
-
-
-@shopping_cart_router.delete(
     "/{user_id}/clean/",
     status_code=status.HTTP_204_NO_CONTENT,
+    summary="Shopping cart cleaner",
+    description="""
+    Clears the user's shopping cart.
+
+    **Validation Rules:**
+    - The **User** must exist.
+    - The **cart_item** must exist in the database.
+
+    **Permissions:**
+    - Users can only clear their **own** cart.
+    - **Moderators** and **Admins** can clear any user's cart.
+    """,
+    responses={
+        400: {
+            "description": "Business logic validation failed",
+            "content": {
+                "application/json": {
+                    "examples": {
+                        "user_not_exist": {
+                            "summary": "user not found",
+                            "value": {"detail": "User not found."},
+                        },
+                        "cart_items_does_not_exist": {
+                            "summary": "cart already empty",
+                            "value": {"detail": "Cart is already empty"},
+                        },
+                    }
+                }
+            },
+        },
+        403: {
+            "description": "Permission denied",
+            "content": {
+                "application/json": {
+                    "example": {"detail": "Not enough permission."}
+                }
+            },
+        },
+    },
 )
 async def delete_cart_items(
     user_id: int,
     authenticated_user: Annotated[CurrentUser, Depends(get_current_user)],
     db: Annotated[AsyncSession, Depends(get_db)],
 ) -> None:
+    """
+    Controller for clearing users cart.
+
+    Arguments:
+        user_id (int): The ID of the target user owner of the cart.
+        authenticated_user (CurrentUser): The user performing the request (from JWT). # noqa: 80
+        db (AsyncSession): Database session.
+    """
     try:
         await clear_cart(
             db=db,
@@ -120,16 +197,134 @@ async def delete_cart_items(
         return
 
 
+@shopping_cart_router.delete(
+    "/{user_id}/{cart_item_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    summary="Remove movie from the shopping cart",
+    description="""
+    Remove a specific movie from the user's shopping cart based on the provided `cart_item_id`.
+
+    **Validation Rules:**
+    - The **User** must exist.
+    - The **cart_item** must exist in the database.
+
+    **Permissions:**
+    - Users can only remove items from their **own** cart.
+    - **Moderators** and **Admins** can remove items from any user's cart.
+    """,  # noqa: 80
+    responses={
+        400: {
+            "description": "Business logic validation failed",
+            "content": {
+                "application/json": {
+                    "examples": {
+                        "user_not_exist": {
+                            "summary": "user not found",
+                            "value": {"detail": "User not found."},
+                        },
+                        "cart_item_does_not_exist": {
+                            "summary": "cart item not found",
+                            "value": {"detail": "Cart item does not exist"},
+                        },
+                    }
+                }
+            },
+        },
+        403: {
+            "description": "Permission denied",
+            "content": {
+                "application/json": {
+                    "example": {"detail": "Not enough permission."}
+                }
+            },
+        },
+    },
+)
+async def delete_cart_item(
+    user_id: int,
+    authenticated_user: Annotated[CurrentUser, Depends(get_current_user)],
+    db: Annotated[AsyncSession, Depends(get_db)],
+    cart_item_id: int,
+) -> None:
+    """
+    Controller for removing a movie from the cart.
+
+    Arguments:
+        user_id (int): The ID of the target user owner of the cart.
+        authenticated_user (CurrentUser): The user performing the request (from JWT). # noqa: 80
+        db (AsyncSession): Database session.
+        cart_item_id (int): The ID of the target cart item.
+    """
+    try:
+        await remove_cart_item(
+            db=db,
+            user_id=user_id,
+            authenticated_user=authenticated_user,
+            cart_item_id=cart_item_id,
+        )
+
+    except (UserNotExist, CartItemDoesNotExist) as error:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail=str(error)
+        )
+    except (UserPermissionDenied,) as error:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail=str(error)
+        )
+
+
 @shopping_cart_router.get(
     "/{user_id}/",
     status_code=status.HTTP_200_OK,
     response_model=CartReadSchema,
+    summary="Shows all movies in cart",
+    description="""
+    Shows movies in user's shopping cart.
+
+    **Validation Rules:**
+    - The **User** must exist.
+
+    **Permissions:**
+    - Users can only read their **own** cart.
+    - **Moderators** and **Admins** can read any user's cart.
+    """,
+    responses={
+        400: {
+            "description": "Business logic validation failed",
+            "content": {
+                "application/json": {
+                    "examples": {
+                        "user_not_exist": {
+                            "summary": "user not found",
+                            "value": {"detail": "User not found"},
+                        },
+                    }
+                }
+            },
+        },
+        403: {
+            "description": "Permission denied",
+            "content": {
+                "application/json": {
+                    "example": {"detail": "Not enough permission."}
+                }
+            },
+        },
+    },
 )
 async def get_cart_items(
     user_id: int,
     authenticated_user: Annotated[CurrentUser, Depends(get_current_user)],
     db: Annotated[AsyncSession, Depends(get_db)],
 ) -> CartReadSchema:
+    """
+    Controller for reading users cart.
+
+    Arguments:
+        user_id (int): The ID of the target user owner of the cart.
+        authenticated_user (CurrentUser): The user performing the request (from JWT). # noqa: 80
+        db (AsyncSession): Database session.
+    """
     try:
         return await get_cart(
             db=db,
@@ -150,12 +345,54 @@ async def get_cart_items(
     "/{user_id}/purchased/",
     status_code=status.HTTP_200_OK,
     response_model=List[MovieInCartSchema],
+    summary="Shows all purchased items",
+    description="""
+    Shows users purchased movies.
+
+    **Validation Rules:**
+    - The **User** must exist.
+
+    **Permissions:**
+    - Users can only read their **own** purchased.
+    - **Moderators** and **Admins** can read any user's purchased.
+    """,
+    responses={
+        400: {
+            "description": "Business logic validation failed",
+            "content": {
+                "application/json": {
+                    "examples": {
+                        "user_not_exist": {
+                            "summary": "user not found",
+                            "value": {"detail": "User not found"},
+                        },
+                    }
+                }
+            },
+        },
+        403: {
+            "description": "Permission denied",
+            "content": {
+                "application/json": {
+                    "example": {"detail": "Not enough permission."}
+                }
+            },
+        },
+    },
 )
 async def get_purchased_cart(
     user_id: int,
     authenticated_user: Annotated[CurrentUser, Depends(get_current_user)],
     db: Annotated[AsyncSession, Depends(get_db)],
 ) -> List[MovieInCartSchema]:
+    """
+    Controller for reading users purchased movies.
+
+    Arguments:
+        user_id (int): The ID of the target user owner of the cart.
+        authenticated_user (CurrentUser): The user performing the request (from JWT). # noqa: 80
+        db (AsyncSession): Database session.
+    """
     try:
         return await get_purchased_items(
             db=db,
