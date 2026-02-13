ee401b9e20cce4499b5642092e31818713b7be1d
Oleh Skibinskyi <oleg.sk2002@gmail.com>
2026-02-13 16:14:32 +0200

    fix: resolved minor query problems
    Resolved problem:
      - superfluous database query on get_cart method
      - n+1 problem in clear_cart method

--- src/crud/shopping_cart.py
+++ src/crud/shopping_cart.py
@@ -1,7 +1,7 @@
 from typing import Annotated, List
 
 from fastapi import Depends
-from sqlalchemy import select
+from sqlalchemy import select, delete
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import selectinload, joinedload
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -116,7 +116,6 @@ async def clear_cart(
     query = (
         select(Cart)
         .where(Cart.user_id == user_id)
-        .options(selectinload(Cart.items))
     )
     result = await db.execute(query)
     cart = result.scalar_one_or_none()
@@ -124,8 +123,8 @@ async def clear_cart(
     if not cart or not cart.items:
         raise CartItemsDoesNotExist("Cart is already empty")
 
-    for item in cart.items:
-        await db.delete(item)
+    delete_query = delete(CartItem).where(CartItem.cart_id == cart.id)
+    await db.scalar(delete_query)
 
     await db.commit()
 
@@ -157,9 +156,7 @@ async def get_cart(
         cart = Cart(user_id=user_id)
         db.add(cart)
         await db.commit()
-
-        result = await db.execute(query)
-        cart = result.scalar_one()
+        await db.refresh(cart)
 
     return CartReadSchema.model_validate(cart)
 
